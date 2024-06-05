2bade0ba56304d45844cb6ba1ae1f7ac5095c97d
Raanthony <raanthony82@gmail.com>
2024-06-05 09:47:08 +0300

    afficher bouton importer

--- resources/views/vendor/backpack/crud/buttons/import_csv.blade.php
+++ resources/views/vendor/backpack/crud/buttons/import_csv.blade.php
@@ -1,13 +1,20 @@
-@if ($crud->hasAccess('create') && backpack_auth()->user()->hasRole('admin') )
-    <div class="card">
-        <form method="post" enctype="multipart/form-data" class="row g-3" action="{{url($crud->route.'/import_csv')}}">
-            @csrf
-            <div class="col-auto">
-                <input type="file"  class="form-control" name="file" accept="text/csv" required/>
+@if ($crud->hasAccess('create') && backpack_auth()->user()->hasRole('root') )
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <form method="post" enctype="multipart/form-data" class="row g-3" action="{{url($crud->route.'/import_csv')}}">
+                        @csrf
+                        <div class="col-auto">
+                            <input type="file"  class="form-control" name="file" accept="text/csv" required/>
+                        </div>
+                        <div class="col-auto">
+                            <button type="submit" class="btn btn-primary mb-3"><span class="ladda-label"><i class="la la-plus"></i></span> Importer fichier CSV</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div class="col-auto">
-                <button type="submit" class="btn btn-primary mb-3"><span class="ladda-label"><i class="la la-plus"></i></span> Importer fichier CSV</button>
-            </div>
-        </form>
+        </div>
+
     </div>
 @endif
